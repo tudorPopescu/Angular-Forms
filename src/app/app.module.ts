20db2b9fb35d846b3862709c15ceb9dd85b980c5
TudorrP <30924442+tudorPopescu@users.noreply.github.com>
2020-04-19 10:58:10 +0300

    1. Template driven forms 3 directives
    TDF directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// import FormsModule
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
