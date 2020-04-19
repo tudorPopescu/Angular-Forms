d7999ab4562a2f89e5e6d5143fbe5e3e9ba9a9f5
TudorrP <30924442+tudorPopescu@users.noreply.github.com>
2020-04-19 16:22:08 +0300

    6. TDF Form Validation
    Form data validation
    Post the form data to a server
    Recieve the response
    Handle any error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { User } from './user';
+// import the EnrollmentService
+import { EnrollmentService } from './enrollment.service';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +11,12 @@ import { User } from './user';
 export class AppComponent {
   public topics = ['Angular', 'ReactJS', 'VueJs'];
   public topicHasError = true;
+  public submitted = false;
+  public errorMsg = '';
 
   userModel = new User('', '', 1234567891, 'default', 'morning', true);
+
+  constructor(private _enrollmentService: EnrollmentService) { }
 
   validateTopic(value) {
     if (value === 'default') {
@@ -18,5 +24,15 @@ export class AppComponent {
     } else {
       this.topicHasError = false;
     }
+  }
+
+  onSubmit() {
+    this.submitted = !this.submitted;
+
+    this._enrollmentService.enroll(this.userModel)
+        .subscribe(
+          data => console.log('Success!', data),
+          error => this.errorMsg = error.statusText
+        )
   }
 }
