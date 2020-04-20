4f2d6296316bd7d6677409d3e8cea78fedc047e8
TudorrP <30924442+tudorPopescu@users.noreply.github.com>
2020-04-20 10:49:50 +0300

    Configured routing to homepage
    Configured routing to homepage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ReactiveFormsComponent } from './reactive-forms/reactive-forms.component';
 import { TemplatedFormsComponent } from './templated-forms/templated-forms.component';
+import { AppComponent } from './app.component';
 
 const routes: Routes = [
+  {path: '' , redirectTo: 'AppComponent', pathMatch: 'full'},
   {path: 'reactive-forms', component: ReactiveFormsComponent},
   {path: 'templated-forms', component: TemplatedFormsComponent}
 ];
